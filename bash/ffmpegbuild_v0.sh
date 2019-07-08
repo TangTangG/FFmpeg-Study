e54b029b4b982b0796858bbeeca375067a04caa6
TangTangG <gutanggu@gmail.com>
2019-07-08 18:05:25 +0800

    bash
    bash

---
+++ bash/ffmpegbuild_v0.sh
@@ -0,0 +1,26 @@
+make clean
+
+export NDK=/mnt/d/own/ffmpeg/android-ndk-r14b
+export PREBUILT=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt
+export PLATFORM=$NDK/platforms/android-18/arch-arm
+export PREFIX=../simplefflib
+build_one(){
+  ./configure --target-os=linux --prefix=$PREFIX \
+--enable-cross-compile \
+--enable-runtime-cpudetect \
+--disable-asm \
+--arch=arm \
+--cc=$PREBUILT/linux-x86_64/bin/arm-linux-androideabi-gcc \
+--cross-prefix=$PREBUILT/linux-x86_64/bin/arm-linux-androideabi- \
+--disable-stripping \
+--nm=$PREBUILT/linux-x86_64/bin/arm-linux-androideabi-nm \
+--sysroot=$PLATFORM \
+--enable-gpl --enable-shared --disable-static --enable-small \
+--disable-ffprobe --disable-ffplay --disable-ffmpeg --disable-debug \
+--extra-cflags="-fPIC -DANDROID -D__thumb__ -mthumb -Wfatal-errors -Wno-deprecated -mfloat-abi=softfp -marm -march=armv7-a" 
+}
+
+build_one
+
+make
+make install
